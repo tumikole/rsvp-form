ff08a3d7baa7a7bf815bd8d3d3677f092a3c5d39
tumikole <ramollosamuel2@gmail.com>
2021-04-28 15:59:08 +0200

    adding html css js  files

---
+++ script.js
@@ -0,0 +1,29 @@
+// var form = [];
+
+
+// var n = 1;
+// var x = 0;
+
+// function submit(event){
+//    event.preventDefault()
+//     // var submit = document.querySelector('#form');
+//     console.log(submit)
+//     var NewRow = submit.insertRow('');
+
+//  let formData = {}
+// formData.name = document.getElementById("fname").value;
+// formData.surname = document.getElementById("lname").value;
+// formData.duties = document.getElementById("duties").value;
+     
+//     var cel1 = NewRow.insertCell(0);
+//     var cel2 = NewRow.insertCell(1);
+//     var cel3 = NewRow.insertCell(2);
+
+//     cel1.innerHTML = formData.name;
+//     cel2.innerHTML = formData.surname;
+//     cel3.innerHTML = formData.duties;
+
+//     n++;
+//     x++;
+    
+// }
